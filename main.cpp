486471a4de4639f9e2b01aed9ce376056fbc1d57
julia <cat ~/em.txt>
2022-04-28 10:31:49 +0100

    Trying impulse resolution just with circles

--- main.cpp
+++ main.cpp
@@ -5,27 +5,22 @@ struct point{
     double x;
     double y;
 };
-struct Rectangle{
-    point min;
-    point max;
-};
 
-struct Circle{
+
+class Circle{
+public:
+    Circle(double r, point c) : (radius = r){}
+    double get_radius() const{return radius;}
+    point& get_centre(){return centre;}
+private:
     double radius;
-    point centre;
+    point& centre;
 };
 
-bool does_rect_intersect(Rectangle& r1, Rectangle& r2){
-    // check if there is separation along the x-axis
-    if (r1.min.x > r2.max.x || r1.max.x < r2.min.x) return false;
-    // check if there is separation along the y-axis
-    if (r1.min.y > r2.max.y || r1.max.y < r2.min.y) return false;
-    return true;
-}
 
 bool does_circle_intersect(Circle& c1, Circle& c2){
-    double distance_sqr = pow(c1.centre.x - c2.centre.x,2) + pow(c1.centre.y - c2.centre.y,2);
-    return pow(c1.radius + c2.radius,2) >= distance_sqr;
+    double distance_sqr = pow(c1.get_centre().x - c2.get_centre().x,2) + pow(c1.get_centre().y - c2.get_centre().y,2);
+    return pow(c1.get_radius() + c2.get_radius(),2) >= distance_sqr;
 }
 
 
